4f53f9fad900842a4119646ccd0195d90d0b84e1
sonuupendra <sonuupendrabhat@gmail.com>
2026-01-01 14:07:59 +0530

    Complete Phase 3: keyword-based risk detection with threshold

--- detector.py
+++ detector.py
@@ -1,5 +1,26 @@
 print("_____PHISHING MAIL DETECTOR_____")
+
+#INPUT STAGE
 text_in_email = input("\nPlease enter the email content below.\n")
 
 print("Analyzing...")
 print("Email length:", len(text_in_email))
+
+text = text_in_email.lower()
+
+#ANALYSIS STAGE
+suspicious_words = ["urgent", "click", "login", "verify"]
+
+found_suspicious_words = 0
+
+for word in suspicious_words:
+    if word in text:
+        found_suspicious_words += 1
+
+print("Suspicious Langauge Indicators found: ", found_suspicious_words)
+
+#DECISION MAKING STAGE
+if found_suspicious_words >= 2:
+    print("LIKELY A PHISHING MAIL")
+else:
+    print("MAIL IS FINE, PLEASE GO AHEAD")

8111d30ebc60f482f77a528df22d6f121566848c
sonuupendra <sonuupendrabhat@gmail.com>
2026-01-03 19:12:55 +0530

    Complete Phase 4: Added clear structure by inculcating functions

--- detector.py
+++ detector.py
@@ -1,26 +1,40 @@
 print("_____PHISHING MAIL DETECTOR_____")
 
 #INPUT STAGE
-text_in_email = input("\nPlease enter the email content below.\n")
-
-print("Analyzing...")
-print("Email length:", len(text_in_email))
-
-text = text_in_email.lower()
+def get_email_text():
+    text_in_email = input("\nPlease enter the email content below.\n")
+    print("Analyzing...")
+    print("Email length:", len(text_in_email))
+    return text_in_email
 
 #ANALYSIS STAGE
-suspicious_words = ["urgent", "click", "login", "verify"]
+def mail_analysis(text):
+    suspicious_words = ["urgent", "click", "login", "verify"]
+    found_suspicious_words = 0
 
-found_suspicious_words = 0
+    for word in suspicious_words:
+        if word in text:
+            found_suspicious_words += 1
+    return found_suspicious_words
 
-for word in suspicious_words:
-    if word in text:
-        found_suspicious_words += 1
 
-print("Suspicious Langauge Indicators found: ", found_suspicious_words)
 
 #DECISION MAKING STAGE
-if found_suspicious_words >= 2:
-    print("LIKELY A PHISHING MAIL")
-else:
-    print("MAIL IS FINE, PLEASE GO AHEAD")
+def risk_analysis(found_suspicious_words):
+    if found_suspicious_words >= 2:
+        return "LIKELY A PHISHING MAIL" 
+       
+    else:
+        return "MAIL IS FINE, PLEASE GO AHEAD"
+
+
+text_in_mail = get_email_text()
+text = text_in_mail.lower()
+
+found_suspicious_words = mail_analysis(text)
+print("Suspicious Langauge Indicators found: ", found_suspicious_words)
+
+risk_decision = risk_analysis(found_suspicious_words)
+print(risk_decision)
+
+    
